1519b145238fa44249d4f57b2a6b528d1ecc19c2
Yadsingh12 <your_donotreply_email_id>
2024-09-04 18:20:35 +0530

    chore: Update server setup with additional middleware and configurations
    - Added express.urlencoded middleware for parsing URL-encoded form data.
    - Included method-override middleware for supporting HTTP method overrides.
    - Updated view directory path to './view'.
    - Set up EJS as the view engine and configured views directory.
    - Organized equire statements for better readability.

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express')
 const articleRouter = require("./routes/articles")
 const Article = require('./models/article')
-const app = express();
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+const app = express()
 
 mongoose.connect('mongodb://localhost/Bharat-intern-Blog-website')
-app.set('view engine', 'ejs');
-
+app.set("views", "./view")
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
+app.use(methodOverride('_method'))
 app.get('/', async(req, res) => {
     const articles =await Article.find().sort({ createdAt:'desc'})
     res.render('articles/index', { articles: articles })
 })
-app.use('/articles',articleRouter)
 
-app.listen(3000);
+app.use('/articles', articleRouter)
+
+app.listen(3000)
