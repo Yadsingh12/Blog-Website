067e1f33a9329662e871224eb0a8b8e657af5c0d
Yadsingh12 <your_donotreply_email_id>
2024-09-04 17:10:18 +0530

    feat: Add MongoDB connection and integrate Article model
    - Added connection to the MongoDB database haratInternDatabase using Mongoose.
    - Integrated the Article model into server.js to handle article-related data operations.

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const articleRouter = require("./routes/articles")
+const Article = require('./models/article')
 const app = express();
 
+mongoose.connect('mongodb://localhost/Bharat-intern-Blog-website')
 app.set('view engine', 'ejs');
 app.get('/', (req, res)=>{
     res.render('index');
