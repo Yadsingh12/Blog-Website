fdc306ecde210ed07cf651c5de0d8d174f997899
Yadsingh12 <your_donotreply_email_id>
2024-09-04 18:03:15 +0530

    feat: Update root route handler to fetch and render articles
    - Articles are now retrieved using Article.find().sort({ createdAt: 'desc' }) to ensure they are sorted by creation date in descending order.
    - Updated the es.render method to pass the fetched articles to the rticles/index EJS template for rendering.

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ const app = express();
 
 mongoose.connect('mongodb://localhost/Bharat-intern-Blog-website')
 app.set('view engine', 'ejs');
-app.get('/', (req, res)=>{
-    res.render('index');
+
+app.get('/', async(req, res) => {
+    const articles =await Article.find().sort({ createdAt:'desc'})
+    res.render('articles/index', { articles: articles })
 })
 app.use('/articles',articleRouter)
 
