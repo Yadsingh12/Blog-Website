3a44a7abfb69d3b649fcf1794d5f9e00c552806e
Yadsingh12 <your_donotreply_email_id>
2024-09-04 16:29:13 +0530

    feat: Integrate article routes into server
    - Added rticleRouter to server.js to handle routes related to articles.
    - Configured the application to use /articles as the base route for article-related requests.

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const articleRouter = require("./routes/articles")
 const app = express();
 
 app.set('view engine', 'ejs');
 app.get('/', (req, res)=>{
     res.render('index');
 })
+app.use('/articles',articleRouter)
 
 app.listen(3000);
